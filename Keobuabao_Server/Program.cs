36d055ee4fc3cc08840fffa91468306baf9426b7
nhatny1 <phamnhatny0804@gmail.com>
2026-01-21 23:23:10 +0700

    code

--- Keobuabao_Server/Program.cs
+++ Keobuabao_Server/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Keobuabao_Server
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Keobuabao_Server
+
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "Kéo Búa Bao Server";
+            TcpListener server = new TcpListener(IPAddress.Any, 8888);
+            server.Start();
+            Console.WriteLine("Server khởi động tại port 8888... Đang chờ người chơi");
+
+            while (true)
+            {
+                // Chờ 2 người chơi
+                TcpClient player1 = server.AcceptTcpClient();
+                SendMessage(player1, "Đang chờ đối thủ...\n");
+
+                TcpClient player2 = server.AcceptTcpClient();
+                SendMessage(player2, "Đang chờ đối thủ...\n");
+
+                // Xử lý trong thread riêng
+                new Thread(() => HandleGame(player1, player2)).Start();
+            }
         }
     }
 }
