be95191120ef4f70bd757311f98e5097ca7ffd4e
nhatny1 <phamnhatny0804@gmail.com>
2026-01-24 18:23:10 +0700

    push code3

--- Keobuabao_Server/Program.cs
+++ Keobuabao_Server/Program.cs
@@ -98,6 +98,25 @@ namespace Keobuabao_Server
             }
         }
         ///2
+        static string ReceiveChoice(TcpClient client)
+        {
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                if (bytesRead == 0)
+                    return null;
+
+                string choice = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+                return (choice == "1" || choice == "2" || choice == "3") ? choice : null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
 
     }
 }
